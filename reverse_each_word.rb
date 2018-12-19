42afaa4d5d070edd371c95e624225d6be2e47748
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 01:41:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,6 @@
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ")
-  reversed.collect do  |i| i.reverse 
+  reversed = sentence.split(" ").collect do  |i| i.reverse 
 end
   reversed.join(" ")
 end
 
-
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |i|
-  i.reverse
-end
-  reversed.join(" ")
-end
